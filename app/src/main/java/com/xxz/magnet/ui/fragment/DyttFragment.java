abbcd696ac4d70e579e0079f25be45be1a16a8d5
joyce <joyceit1991@gmail.com>
2016-12-26 08:20:47 +0800

    临时提交

---
+++ app/src/main/java/com/xxz/magnet/ui/fragment/DyttFragment.java
@@ -0,0 +1,22 @@
+package com.xxz.magnet.ui.fragment;
+
+import android.view.View;
+
+import com.xxz.magnet.R;
+
+/**
+ * 电影天堂类型电影详情页面
+ * 1.
+ */
+
+public class DyttFragment extends BaseFragment {
+    @Override
+    public void bindViews(View view) {
+
+    }
+
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.fragmant_detail_dytt;
+    }
+}
